fc67ac96d728b770acde3dba1d3c26313c4552a6
canaleija <robanac@gmail.com>
2024-10-22 08:11:12 -0600

    practica arreglos

---
+++ arreglos/arreglos1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int arreglo [] = {6,5,4,3,2,1};
+    for(x=0;x<5;x++){
+        printf("%d\n", arreglo[x]);
+    }
+    /// se implementar un ordenamiento de datos Burbuja
+
+
+
+    // mostrar los datos en consola, verificar que los elementos 
+    // en el arreglo estan ordenados de forma Asc
+
+
+    
+    return 0;
+}

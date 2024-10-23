781d1c70b5b71c840125b264ebad5fa2436ae530
canaleija <robanac@gmail.com>
2024-10-23 13:54:40 -0600

    burbuja en main

--- arreglos/arreglos1.c
+++ arreglos/arreglos1.c
@@ -2,18 +2,27 @@
 
 int main()
 {
-    int x;
+    int x,i,j, temp;
     int arreglo [] = {6,5,4,3,2,1};
-    for(x=0;x<5;x++){
-        printf("%d\n", arreglo[x]);
-    }
+    
     /// se implementar un ordenamiento de datos Burbuja
-
+    for(i=1;i<6;i++){ ///  son las veces que tiene que subir la burbuja
+        for(j=0;j<6-i;j++){// son los intercambios para subir cada burbuja
+            if(arreglo[j]>arreglo[j+1]){// si cumple que es mayor
+                // intercambio
+                temp = arreglo[j];
+                arreglo[j]=arreglo[j+1];
+                arreglo[j+1] = temp;
+            }
+        }
+    }
 
 
     // mostrar los datos en consola, verificar que los elementos 
     // en el arreglo estan ordenados de forma Asc
-
+    for(x=0;x<5;x++){
+        printf("%d\n", arreglo[x]);
+    }
 
     
     return 0;

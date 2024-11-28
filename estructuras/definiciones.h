13ec6161de6852cfe418ae409d02b93e86d6bdfe
canaleija <robanac@gmail.com>
2024-11-28 12:02:03 -0600

    pre curp

---
+++ estructuras/definiciones.h
@@ -0,0 +1,9 @@
+struct fecha
+{
+    /* todos los datos que de alguna se concluyen que describen a un fecha convencial*/
+    int dia;
+    int mes;
+    int anio;
+};
+
+void imprimir();
--- estructuras/estructura1.c
+++ estructuras/estructura1.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include "definiciones.h"
+
+
 
 struct fecha
 {
-    /* todos los datos que de alguna se concluyen que describen a un fecha convencial*/
     int dia;
     int mes;
     int anio;
 };
-
-
-
-int main(int argc, char const *argv[])
+struct ciudadano
 {
-    struct fecha cumple_beto;
-    //struct fecha cumple_beto;
+    char nombre_completo[40];
+    char a_paterno[20];
+    char a_materno[20];
+    struct fecha fecha_nac;
+    char edo_nac [25];
+    char sexo;
+    char curp [30];
 
-    struct fecha fechas[] = {cumple_beto};
-   
-    
-    cumple_beto.dia = 29;
-    cumple_beto.mes = 7;
-    cumple_beto.anio = 1987;
+    /* data */
+};
 
-    printf("%d",cumple_beto.dia*cumple_beto.mes);
 
-    
-    
+int main()
+{
+    struct ciudadano beto;
+
+    strcpy(beto.nombre_completo,"Roberto Oswaldo");
+    strcpy(beto.a_paterno,"Cruz");
+    strcpy(beto.a_materno,"Leija");
+    beto.fecha_nac.dia = 29;
+    beto.fecha_nac.mes = 7;
+    beto.fecha_nac.anio = 2003;
+    beto.sexo = 'H';
+    strcpy(beto.edo_nac,"ZACATECAS");
+    // tarea
+    beto.curp = // funcion (ciudadano)
+
+
+
     return 0;
 }
-
-
-// nombre
-// apellido p
-// apellido m
-// 
-// fecha de nacimiento   
-// ciudad
-// curp 
